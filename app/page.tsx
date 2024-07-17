1d214fde7f18de55ce3c7b74ab6c943194f26f5d
heriberto2300 <joalonsom@hotmail.com>
2024-07-16 22:18:00 -0600

    Commit de correccion #3: Eliminacion de animaciones

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,8 @@ import Projects from "@/components/sections/projects";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <BoxesSpin />
-      <div className="flex min-h-screen flex-col items-center justify-between absolute z-10">
-        <Presentation />
-        <Projects />
-      </div>
+      <Presentation />
+      <Projects />
     </main>
   );
 }
